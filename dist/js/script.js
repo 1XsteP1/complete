0fd8ce882f7840e7f2606f47fe08aaa29faabf82
artem <forroyalee2@gmail.com>
2020-10-14 16:50:31 +0300

    start

---
+++ dist/js/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const ruBtn = document.getElementsByClassName('header__ru'),
+    const engBtn = document.getElementsByClassName('header__eng');
+    const isRuClicked = true
+
+    ruBtn[0].addEventListener('click', (elem) => {
+        console.log(ruBtn)
+    })
+})
